ceaddbd4c8b87f050be251586701a7290c611a7a
Jessica <36334025+Marline123@users.noreply.github.com>
2021-01-02 17:34:47 -0600

    Add files via upload

--- EliRecursionIzq.java
+++ EliRecursionIzq.java
@@ -33,34 +33,70 @@ public class EliRecursionIzq {
 			line = reader.readLine();
 			int c=1;
 			while(c<contline){
+				boolean ban=false;
+				boolean prim=false;
 				String[] splitLine = line.trim().split("\\s+");
 				int sizeSL = splitLine.length;
-					//System.out.println("count: "+sizeSL);
-					writer.write(splitLine[0]+" "+splitLine[1]+" ");
-			
-				String simb = splitLine[sizeSL-1]+splitLine[0]+"'";
-					//System.out.println("simb: "+simb);
-				  writer.write(simb+"\n"+splitLine[0]+"' "+splitLine[1]+" ");
-				 
-				for(int i=2; i<sizeSL-1; i=i+2){	
-					String[] sim = splitLine[i].split("");
-					int len = sim.length;										
-						//System.out.println("sim: "+sim[1]+" , len: "+len);
-					
-					if(len==3)	// EJ. E+T
-						writer.write(sim[1]+simb+"| ");
-					else if(len==2) //EJ. E+
-						writer.write(sim[1]+sim[0]+"| ");
-					else{		//EJ. EorT
-						String sim2="";
-						for(int j=1; j<len-1; j++){
-							sim2=sim2+sim[j];
-						}
-						writer.write(sim2+simb+"| ");
+				
+				
+				
+				for(int i=2; i<sizeSL; i=i+2){
+					String[] sepPriR = splitLine[i].split("");
+					if(splitLine[0].equals(sepPriR[0])){
+						ban=true;
+						if(i==2)
+							prim=true;
 					}
 				}
-				writer.write("ep");
 				
+				
+				/*cuando necesita recursion*/
+				if(ban){
+					
+				
+					//System.out.println("count: "+sizeSL);
+					writer.write(splitLine[0]+" "+splitLine[1]+" "); // E -> 
+					String simb = "";
+					if(prim){
+						simb = splitLine[sizeSL-1]+splitLine[0]+"'";
+							System.out.println("simb: "+simb);
+						  writer.write(simb+"\n"+splitLine[0]+"' "+splitLine[1]+" ");
+					}else{
+						String[] sep = splitLine[4].split("");
+						String pa="";
+						for(int j=1; j<sep.length; j++){
+							pa=pa+sep[j];
+						}
+						//simb = pa+splitLine[0]+"'";
+						simb= sep[sep.length-1]+sep[0]+"'";
+						writer.write(simb+"\n"+splitLine[0]+"' "+splitLine[1]+" ");
+					}	 
+				
+					for(int i=2; i<sizeSL; i=i+2){
+								System.out.println("\ti:"+i);
+						String[] sim = splitLine[i].split("");
+						int len = sim.length;										
+							System.out.println("\tsplitline: "+splitLine[i]+" , len: "+len);
+						
+						 if(len==2) //EJ. E+
+							writer.write(sim[1]+sim[0]+"' | ");
+						else if(len==1)
+							writer.write("");
+						else{		//EJ. EorT
+							String sim2="";
+							for(int j=1; j<len-1; j++){
+								sim2=sim2+sim[j];
+							}
+					
+							writer.write(sim2+sim[len-1]+sim[0]+"' | ");
+						
+							
+						}
+					}
+					writer.write("ep");
+				}else{
+					writer.write(line);
+				}
 				
 				// read next line
 				line = reader.readLine();
