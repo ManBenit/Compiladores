72a6140e6f3aa00820ac2fac4b37e0ab85719288
ManBenit <benitez.emilio.morales@gmail.com>
2021-01-09 00:32:52 -0600

    habemus first y follow

--- Compilador/src/AnalizadorSintactico/CreadorAnaLex.java
+++ Compilador/src/AnalizadorSintactico/CreadorAnaLex.java
@@ -1,32 +1,25 @@
 package AnalizadorSintactico;
 
-import AnalizadorLexico.AFD;
-import AnalizadorLexico.AFN;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
 
 public class CreadorAnaLex {
-//    private LinkedList<String> ladoIzquierdo;
-//    private LinkedList<String> ladoDerecho;
-    private boolean flagTerminal=false; //true: terminal, false: no terminal
-    //private LinkedHashMap< String, Map<Integer, LinkedList<String>> > gramLeida;
     private LinkedHashMap< String, LinkedList<Object[]> > gramLeida;
     private int numeroRegla=0; //Indica la cantidad de reglas que hay
+    private String[][] tablaLL1;
+    private HashSet<String> alfaGram; //"alfabeto" de la gramática (obtener los símbolos necesarios para la tabla LL1)
     
     public CreadorAnaLex(String ruta){
         gramLeida= new LinkedHashMap();
         mapearGramatica(ruta);
+//        cargarSimbolos(); //Para hacer tabla LL1
+//        for(String x: alfaGram)
+//            System.out.println(x);
         
         int nr=0;
         String r="";
@@ -48,7 +41,6 @@ public class CreadorAnaLex {
     
     private void mapearGramatica(String ruta){
         numeroRegla=1;
-        boolean elBool;
         try{
             BufferedReader br= new BufferedReader(new FileReader(new File(adaptarRuta(ruta))));
             
@@ -68,73 +60,268 @@ public class CreadorAnaLex {
                     auxList.add(mapa);
                     numeroRegla+=1;
                 }
-                gramLeida.put(splitline[0], auxList);
+                gramLeida.put(splitline[0].trim(), auxList);
                 
             } 
-            firstest();
+            firstest(); //debemos quitar esta invocación de aquí
             br.close();
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
         }
     }
     
+    private void cargarSimbolos(){
+        alfaGram= new HashSet<>(); //Alfabeto de la gramática
+        StringBuilder sb= new StringBuilder();
+        
+        for(String llave: gramLeida.keySet()){
+            LinkedList<Object[]> lista= gramLeida.get(llave);
+            char[] charArray;
+            int aux;
+            for(int i=0; i<lista.size(); i++){
+                Object[] ao= lista.get(i);
+                charArray= ((String)ao[1]).toCharArray();
+                for(char c: charArray)
+                    System.out.print(c+", ");
+                System.out.println("");
+                
+                for(int j=0; j<charArray.length; j++){
+                    System.out.println("aux"+j);
+                    if(esTerminal(charArray[j]) && charArray[j]!='\''){
+                        System.out.println("entrado");
+                        if((int)charArray[j]>=97 && (int)charArray[j]<=122){
+                            System.out.println("esmin");
+                            aux= j;
+                            System.out.println("j="+j);
+                            System.out.println("char0"+charArray[aux]);
+                            while((int)charArray[aux]>=97 && (int)charArray[aux]<=122){
+                                sb.append(charArray[aux]);
+                                aux+=1;
+                            }
+                            j= aux;
+                            alfaGram.add(sb.toString());
+                            sb.delete(0, sb.length());
+                        }
+                        else
+                            alfaGram.add(String.valueOf(charArray[j]));
+                    }
+                    else
+                        System.out.println("nel karnal: "+charArray[j]);
+                }
+            }
+        }
+    }
+    
+    private String[] first(String nodo){
+        int tam= nodo.toCharArray().length;
+        String[] retFirst= new String[25];
+        int posRet=0; //Para asignar las posiciones de retFirst
+        StringBuilder sb= new StringBuilder("");
+        
+        if(esTerminal(nodo.charAt(0))){ //Caso donde el primer símbolo (de 1 o más caracteres) es un terminal
+            for(int j=0; j<nodo.toCharArray().length; j++){
+                if(esTerminal(nodo.charAt(j))){
+                    sb.append(nodo.charAt(j));
+                }
+                else
+                    break;
+            }
+            retFirst[posRet]=sb.toString();
+        }
+        else{ //Caso donde se busca dentro de las reglas, el first del símbolo hasta llegar a un terminal
+            LinkedList<Object[]> lista;
+            if(tam==2 && nodo.charAt(1)=='\'') //Lado izquierdo de un solo caracter (X), X=cualquier no terminal
+                lista= gramLeida.get(nodo);
+            else
+                lista= gramLeida.get( String.valueOf(nodo.charAt(0)) );
+
+            for(int i=0; i<lista.size(); i++){
+                Object[] ao= lista.get(i);
+
+                char primerSim= ((String)ao[1]).charAt(0);
+                if( esTerminal(primerSim) ){
+
+                    //Ciclo por si a caso se trata de un símbolo de varios caracteres
+                    for(int j=0; j<((String)ao[1]).toCharArray().length; j++){
+                        if(esTerminal(((String)ao[1]).charAt(j))){
+                            //Si es minúscula, es símbolo de varios caracteres, entonces concatena todos esos caracteres
+                            if( (int)((String)ao[1]).charAt(j)>=97 && (int)((String)ao[1]).charAt(j)<=122 )
+                                sb.append(((String)ao[1]).charAt(j));
+                            else{ //Si es un caracter diferente a minúsculas, obtenlo y sal del ciclo
+                                sb.append(((String)ao[1]).charAt(j)); //Solo lleja a j=0, es decir, solo el primer caracter
+                                break;
+                            }
+                        }
+                    }
+                    retFirst[posRet]= String.valueOf(sb.toString());
+                    posRet+=1;
+                    sb.delete(0, sb.length());
+                }
+                else{
+                    retFirst= first(String.valueOf(primerSim));
+                }
+            }
+        }
+        
+        return retFirst;
+    }
+    
+    private HashSet<String> follow(String A){
+        HashSet<String> R= new HashSet<>();
+        String[] aux;
+        String[] auxArray;
+        Object[] N=null;//Modificar
+        for(int i=1; i<numeroRegla-1; i++){
+            N= buscarSimbolo(A, i); //revLista.get(j);
+            if(N!=null){
+                auxArray= ((String)N[1]).split(A);
+                if(auxArray.length==1){ //Si es el último símbolo de la regla
+                    System.out.println("simon karnal");
+                    if(A.equals( obtLlavePorLadDer( (String)N[1] ) ))//ArrReglas[i].strSimb
+                        continue;
+                    R.addAll(follow( obtLlavePorLadDer( (String)N[1] ) ));
+                }
+                else{
+                    aux= first(auxArray[1]); //Siguiente símbolo del símbolo analizado (A)
+                    for(String auxF: aux){
+                        if(auxF!=null){
+                            if(auxF.contains("ep")){
+                                auxF= auxF.replace("ep", "\u0190");
+                                R.add(auxF);
+                                if(!A.equals(aux))
+                                    R.addAll(follow(auxF));
+                            }
+                            else
+                                R.add(auxF);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return R;
+    }
+    
+    private String firstest(){    
+//        Object[] o= buscarSimbolo("ep", 8); // 1    TE' false
+//        System.out.print("elNodo:\t");
+//        System.out.print((int)o[0]);
+//        System.out.print("\t"+(String)o[1]);
+//        System.out.println("\t"+(boolean)o[2]);
+
+//        //for(int i=0; i<numeroRegla-1; i++){
+//            String encontrado= buscarIzqEnDer("T'", 5);
+//            if(!encontrado.equals(""))
+//                System.out.println(encontrado);
+//        //}
+        
+//        System.out.println(obtLlavePorLadDer("FT'"));
+        
+//        for(String s: first("F"))
+//            if(s!=null)
+//                System.out.println("elFirst: "+s);
+
+        for(String s: follow("F"))
+            System.out.println("elFollow: "+s);
+
+        
+        return "";
+    }
+    
     private boolean esTerminal(char c){
         return !((int)c >= 65 && (int)c <= 90);
     }
     
-    private HashSet<String> first(String nodo){
-        HashSet<String> R=null;
-        String aux;
-        
-        if( esTerminal(nodo.charAt(0)) || (int)nodo.charAt(0)==400 ){ //400 es el Unicode de épsilon
-            R.add(nodo);
-            return R;
-        }
-        return null;
+    //Devuelve el lado derecho que contenga a 'buscar' en la regla numReg
+    private String buscarIzqEnDer(String buscar, int numReg){
+        Object[] aux= buscarSimbolo(buscar, numReg);
+        if( aux==null )
+            return "";
+        else
+            return (String) aux[1];
     }
- 
+    
+    private String obtLlavePorLadDer(String ladDer){
+        //obtIzqPorDer, con Der String, sin saber numRegla (obtener la llave de Der)
+        String llave="";
+        int in=0;
+        for(String izq: gramLeida.keySet()){
+            LinkedList<Object[]> lista= gramLeida.get(izq);
+            for(int i=0; i<lista.size(); i++){
+                Object[] ao= lista.get(i);
+                if( ((String)ao[1]).equals(ladDer) ){
+                    llave= izq;
+                    in=numeroRegla-1;
+                    break;
+                }
+            }
+            if(in==numeroRegla-1)
+                break;
+            in+=1;
+        }
+        
+        return llave;
+    }
+    
+    //Buscar el símbolo (llave) "entra" en la regla número numreg
+    private Object[] buscarSimbolo(String entra, int numreg){
+        int tam= entra.toCharArray().length;
+        boolean contenido;
+        
+        Object[] retNodo=null;
+        int auxIndex=0;
+        for(String llave: gramLeida.keySet()){
+            LinkedList<Object[]> lista= gramLeida.get(llave);
+            for(int i=0; i<lista.size(); i++){
+                Object[] ao= lista.get(i);
+                
+                String regla= (String)ao[1];
+                int num= (int)ao[0];
+                
+                if(tam==2) //si tiene prima, solo confirma si contiene la cadena
+                    contenido= regla.contains(entra);
+                else //si no contiene prima, verifica que lo contenga y además que el siguiente caracter no sea la prima
+                    contenido= regla.contains(entra) && regla.charAt( regla.indexOf(entra)+1 )!='\'';
+                
+                if(contenido && num==numreg){
+                    //Como X' (X= cualquier no terminal) está siempre al final, puede darse el caso donde entra=X, por lo que
+                    //se detectaría que contains es true si la regla es alfaX', eso implica un error, así que si
+                    //entra=X cuando la regla es alfaX', hay que ignorar el índice del apóstrofe
+                    retNodo= ao;
+                    auxIndex= gramLeida.size()-1;
+                    break;
+                }
+            }
+            
+            if(auxIndex==gramLeida.size()-1)
+                break;
+            
+            auxIndex+=1;
+        }
+        return retNodo;
+    }
+    
+    private String obtReglaPorNumero(int numeroDeRegla){
+        String regla="";
+        
+        for(String llave: gramLeida.keySet()){
+            LinkedList<Object[]> lista= gramLeida.get(llave);
+            for(int i=0; i<lista.size(); i++){
+                Object[] ao= lista.get(i);
+                if( (int)ao[0]==numeroDeRegla ){
+                    regla= (String)ao[1];
+                    break;
+                }
+            }
+        }
+        
+        return regla;
+    }
+    
     private String adaptarRuta(String ruta){
         if(System.getProperty("os.name").equals("Windows"))
             return ruta.replace("/", "\\");
         else
             return ruta;
-    }
-    
-    private String firstest(){        
-        
-        StringBuilder sb= new StringBuilder("");
-        for(String llave: gramLeida.keySet()){
-            LinkedList<Object[]> lista= gramLeida.get(llave);
-            for(int i=0; i<lista.size(); i++){
-                Object[] ao= lista.get(i);
-
-
-                    if((boolean)ao[2]){
-                        String regla= (String)ao[1];
-                        
-                        //Obtener solo operadores
-                        for(int j=0; j<regla.toCharArray().length; j++){
-                            if(esTerminal(regla.charAt(j))){
-                                sb.append(regla.charAt(j));
-                            }
-                            else
-                                break;
-                        }
-                        System.out.println("Regla #"+(int)ao[0]+" -> "+sb.toString());
-                        if(sb.toString().equalsIgnoreCase("ep")){
-                            System.out.println("Entrar al follow");
-                            //Cuchau :v
-                            
-                        }else{
-                            System.out.println("Entratr al first");
-                        }
-                        sb.delete(0, sb.length());
-                    }
-            }
-            sb.delete(0, sb.length());
-        }
-        
-        
-        return "";
     }
 }
--- Compilador/src/AnalizadorSintactico/EliRecursionIzq.java
+++ Compilador/src/AnalizadorSintactico/EliRecursionIzq.java
@@ -1,84 +1,119 @@
-package AnalizadorSintactico;
-
+package analizadorsintactico;
 import java.io.*;
 
 
 public class EliRecursionIzq {
 
-    public static void main(String[] args) {
-        BufferedReader reader;
-        BufferedWriter writer;
-        try {
-            /*crea un archivo*/
-            File Nfile = new File("gramnorec.txt");
-            if (Nfile.createNewFile()) {
-                System.out.println("File created: " + Nfile.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-
-            reader = new BufferedReader(new FileReader(adaptarRuta("../gramrec.txt")));
-            writer = new BufferedWriter(new FileWriter(adaptarRuta("../gramnorec.txt")));
-
-            /*para obtener el numero de filas*/
-
-            String line = reader.readLine();
-            int contline = 0;
-            while (line != null) {
-                System.out.println(line);
-                line = reader.readLine();
-                contline++;
-            }
-            //System.out.println("contline: "+contline);
-
-            reader = new BufferedReader(new FileReader(adaptarRuta("../gramrec.txt")));
-            line = reader.readLine();
-            int c=1;
-            while(c<contline){
-                String[] splitLine = line.trim().split("\\s+");
-                int sizeSL = splitLine.length;
-                //System.out.println("count: "+sizeSL);
-                writer.write(splitLine[0]+" "+splitLine[1]+" ");
-
-                String simb = splitLine[sizeSL-1]+splitLine[0]+"'";
-                //System.out.println("simb: "+simb);
-                writer.write(simb+"\n"+splitLine[0]+"' "+splitLine[1]+" ");
-
-                for(int i=2; i<sizeSL-1; i=i+2){	
-                    String[] sim = splitLine[i].split("");
-                    int len = sim.length;										
-                    //System.out.println("sim: "+sim[1]+" , len: "+len);
-
-                    if(len==3)	// EJ. E+T
-                        writer.write(sim[1]+simb+"| ");
-                    else if(len==2) //EJ. E+
-                        writer.write(sim[1]+sim[0]+"| ");
-                    else{		//EJ. EorT
-                        String sim2="";
-                        for(int j=1; j<len-1; j++){
-                            sim2=sim2+sim[j];
-                        }
-                        writer.write(sim2+simb+"| ");
-                    }
-                }
-                writer.write("ep");
-
-
-                // read next line
-                line = reader.readLine();
-                writer.write("\n");
-                c++;
-            }
-            writer.write(line);
-
-
-            reader.close();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
+	public static void main(String[] args) {
+		BufferedReader reader;
+		BufferedWriter writer;
+		try {
+			/*crea un archivo*/
+			File Nfile = new File(adaptarRuta("../gramnorec.txt"));
+			if (Nfile.createNewFile()) {
+				System.out.println("File created: " + Nfile.getName());
+			} else {
+				System.out.println("File already exists.");
+			}
+				
+			reader = new BufferedReader(new FileReader(adaptarRuta("../gramrec.txt")));
+			writer = new BufferedWriter(new FileWriter(adaptarRuta("../gramnorec.txt")));
+			
+			/*para obtener el numero de filas*/
+			
+			String line = reader.readLine();
+			int contline = 0;
+			while (line != null) {
+				System.out.println(line);
+				line = reader.readLine();
+				contline++;
+			}
+			//System.out.println("contline: "+contline);
+			
+			reader = new BufferedReader(new FileReader(adaptarRuta("../gramrec.txt")));
+			line = reader.readLine();
+			int c=1;
+			while(c<contline){
+				boolean ban=false;
+				boolean prim=false;
+				String[] splitLine = line.trim().split("\\s+");
+				int sizeSL = splitLine.length;
+				
+				
+				
+				for(int i=2; i<sizeSL; i=i+2){
+					String[] sepPriR = splitLine[i].split("");
+					if(splitLine[0].equals(sepPriR[0])){
+						ban=true;
+						if(i==2)
+							prim=true;
+					}
+				}
+				
+				
+				/*cuando necesita recursion*/
+				if(ban){
+					
+				
+					//System.out.println("count: "+sizeSL);
+					writer.write(splitLine[0]+" "+splitLine[1]+" "); // E -> 
+					String simb = "";
+					if(prim){
+						simb = splitLine[sizeSL-1]+splitLine[0]+"'";
+							System.out.println("simb: "+simb);
+						  writer.write(simb+"\n"+splitLine[0]+"' "+splitLine[1]+" ");
+					}else{
+						String[] sep = splitLine[4].split("");
+						String pa="";
+						for(int j=1; j<sep.length; j++){
+							pa=pa+sep[j];
+						}
+						//simb = pa+splitLine[0]+"'";
+						simb= sep[sep.length-1]+sep[0]+"'";
+						writer.write(simb+"\n"+splitLine[0]+"' "+splitLine[1]+" ");
+					}	 
+				
+					for(int i=2; i<sizeSL; i=i+2){
+								System.out.println("\ti:"+i);
+						String[] sim = splitLine[i].split("");
+						int len = sim.length;										
+							System.out.println("\tsplitline: "+splitLine[i]+" , len: "+len);
+						
+						 if(len==2) //EJ. E+
+							writer.write(sim[1]+sim[0]+"' | ");
+						else if(len==1)
+							writer.write("");
+						else{		//EJ. EorT
+							String sim2="";
+							for(int j=1; j<len-1; j++){
+								sim2=sim2+sim[j];
+							}
+					
+							writer.write(sim2+sim[len-1]+sim[0]+"' | ");
+						
+							
+						}
+					}
+					writer.write("ep");
+				}else{
+					writer.write(line);
+				}
+				
+				// read next line
+				line = reader.readLine();
+					writer.write("\n");
+				c++;
+			}
+			writer.write(line);
+			
+			
+			reader.close();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+        
     private static String adaptarRuta(String ruta){
         if(System.getProperty("os.name").equals("Windows"))
             return ruta.replace("/", "\\");
